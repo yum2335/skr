56eeb405a98b2b391677982a168ac739f50ad117
yum <syungoriradesu@gmail.com>
2023-01-25 15:48:04 +0900

    skr

---
+++ SEEDCmdChecker/houkou.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeedCmdChecker
+{
+    class houkou
+    {
+        private int x = System.Windows.Forms.Cursor.Position.X;
+        private int y = System.Windows.Forms.Cursor.Position.X;
+
+        public int X
+        {
+            get { return x; }
+        }
+        public int Y
+        {
+            get { return y; }
+        }
+
+
+
+
+    }
+}
